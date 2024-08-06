ddf941082dd033f96310196984f7f67c4f817d5d
hongnv <hongnv@phugroup.com>
2024-08-06 16:15:33 +0700

    Create voxel map

---
+++ Assets/Scripts/VoxelGrid.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour
+{
+    public GameObject voxelPrefab;
+    public int resolution;
+    private float _voxelSize;
+    private bool[] _voxels;
+    private Material[] _voxelMaterials;
+
+    public void Initialize(int initResolution, float initSize)
+    {
+        resolution = initResolution;
+        _voxelSize = initSize / resolution;
+        var length = resolution * resolution;
+        _voxels = new bool[length];
+        _voxelMaterials = new Material[length];
+
+        for (int i = 0, y = 0; y < initResolution; y++)
+        {
+            for (var x = 0; x < initResolution; x++, i++)
+            {
+                CreateVoxel(i, x, y);
+            }
+        }
+
+        SetVoxelColors();
+    }
+
+    private void CreateVoxel(int i, int x, int y)
+    {
+        var o = Instantiate(voxelPrefab, transform, true);
+        o.transform.localPosition = new Vector3((x + 0.5f) * _voxelSize, (y + 0.5f) * _voxelSize);
+        var voxelScale = Vector3.one * _voxelSize * 0.9f;
+        o.transform.localScale = voxelScale;
+        _voxelMaterials[i] = o.GetComponent<MeshRenderer>().material;
+    }
+
+    public void SetVoxel(int x, int y, bool state)
+    {
+        _voxels[y * resolution + x] = state;
+        SetVoxelColors();
+    }
+
+    private void SetVoxelColors()
+    {
+        for (var i = 0; i < _voxels.Length; i++)
+        {
+            _voxelMaterials[i].color = _voxels[i] ? Color.black : Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/VoxelMap.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public class VoxelMap : MonoBehaviour
+{
+    public float size = 2f;
+    public int voxelResolution = 8;
+    public int chunkResolution = 2;
+    public VoxelGrid voxelGridPrefab;
+
+    private VoxelGrid[] _chunks;
+    private float _chunkSize, _voxelSize, _halfSize;
+
+    #region Unity Event Functions
+
+    private void Awake()
+    {
+        _halfSize = size * 0.5f;
+        _chunkSize = size / chunkResolution;
+        _voxelSize = _chunkSize / voxelResolution;
+
+        _chunks = new VoxelGrid[chunkResolution * chunkResolution];
+        for (int i = 0, y = 0; y < chunkResolution; y++)
+        {
+            for (var x = 0; x < chunkResolution; x++, i++)
+            {
+                CreateChunk(i, x, y);
+            }
+        }
+
+        var box = gameObject.AddComponent<BoxCollider>();
+        box.size = new Vector3(size, size);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo))
+            {
+                if (hitInfo.collider.gameObject == gameObject)
+                {
+                    EditVoxels(transform.InverseTransformPoint(hitInfo.point));
+                }
+            }
+        }
+    }
+
+    private void EditVoxels(Vector3 point)
+    {
+        var voxelX = (int)((point.x + _halfSize) / _voxelSize);
+        var voxelY = (int)((point.y + _halfSize) / _voxelSize);
+        var chunkX = voxelX / voxelResolution;
+        var chunkY = voxelY / voxelResolution;
+
+        voxelX -= chunkX * voxelResolution;
+        voxelY -= chunkY * voxelResolution;
+        _chunks[chunkY * chunkResolution + chunkX].SetVoxel(voxelX, voxelY, true);
+        Debug.Log($"{voxelX}, {voxelY} in chunk {chunkX}, {chunkY}");
+    }
+
+    #endregion
+
+
+    private void CreateChunk(int i, int x, int y)
+    {
+        var chunk = Instantiate(voxelGridPrefab, transform, true);
+        chunk.Initialize(voxelResolution, _chunkSize);
+        chunk.transform.localPosition = new Vector3(x * _chunkSize - _halfSize, y * _chunkSize - _halfSize);
+        _chunks[i] = chunk;
+    }
+}
